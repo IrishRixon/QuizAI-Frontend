6b106c6838ae71bf5463e5866490008e14b6aa41
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-10-20 21:49:41 +0800

    initial commit

---
+++ src/Components/LandingPage/LandingPage.tsx
@@ -0,0 +1,9 @@
+function LandingPage() {
+  return (
+    <div className="h-screen w-screen bg-(--primary-color) relative">
+      <div className="absolute h-screen w-screen bg-[url(/images/robot.png)] bg-repeat-round bg-[length:80px_80px] opacity-30"></div>
+    </div>
+  );
+}
+
+export default LandingPage;
