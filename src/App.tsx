fb5a347441dbe290a0fcbe35cfe7005a559d19bf
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-10-21 17:10:13 +0800

    feat: add routing with react-router and implement navigation to categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router'
 import './App.css'
 import LandingPage from './Components/LandingPage/LandingPage'
+import Categories from './Components/Categories/Categories'
 
 function App() {
 
   return (
     <>
-      <LandingPage></LandingPage>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LandingPage />}/>
+        <Route path='/categories' element={<Categories />}/>
+        <Route path='*' element={<h1>Page not found</h1>}/>
+      </Routes>
+    </BrowserRouter>
     </>
   )
 }
--- src/Components/LandingPage/Button.tsx
+++ src/Components/LandingPage/Button.tsx
@@ -1,10 +1,15 @@
+import { useNavigate } from "react-router";
+
 interface ButtonProps {
   txt: string;
 }
 
 function Button(props: ButtonProps) {
+    let navigate = useNavigate();
+
   return (
-    <button className="px-8 py-4 text-(--text-color) relative border-2 border-(--secondary-color) rounded-sm w-[178px] text-xl text-center whitespace-nowrap hover:bg-(--secondary-color) active:bg-(--secondary-color) transition">
+    <button className="px-8 py-4 text-(--text-color) relative border-2 border-(--secondary-color) rounded-sm w-[178px] text-xl text-center whitespace-nowrap hover:bg-(--secondary-color) active:bg-(--secondary-color) transition" 
+    onClick={() => navigate("/categories")}>
       {props.txt}
     </button>
   );
