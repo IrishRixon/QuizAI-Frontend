bf8ffb79fefa340b771415bc8bb47041c87746e5
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-10-21 22:07:33 +0800

    feat: add Material Symbols font and enhance categories component layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,24 @@
-import { BrowserRouter, Route, Routes } from 'react-router'
-import './App.css'
-import LandingPage from './Components/LandingPage/LandingPage'
-import Categories from './Components/Categories/Categories'
+import { BrowserRouter, Route, Routes } from "react-router";
+import "./App.css";
+import LandingPage from "./Components/LandingPage/LandingPage";
+import Categories from "./Components/Categories/Categories";
 
 function App() {
-
   return (
     <>
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<LandingPage />}/>
-        <Route path='/categories' element={<Categories />}/>
-        <Route path='*' element={<h1>Page not found</h1>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <div className="h-screen w-screen bg-(--primary-color) relative">
+          <div className="absolute h-screen w-screen bg-[url(/images/robot.png)] bg-repeat-round bg-[length:80px_80px] opacity-30 -z-0"></div>
+
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="*" element={<h1>Page not found</h1>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Components/Categories/Categories.tsx
+++ src/Components/Categories/Categories.tsx
@@ -1,8 +1,15 @@
-
 function Categories() {
   return (
-    <h1>Categories</h1>
-  )
+    <div className="h-full w-full p-6 relative z-10">
+      <button type="button" className="flex active:bg-(--secondary-color) active:text-(--white-text) rounded-sm">
+        <span className="material-symbols-rounded" style={{fontSize:'40px', color:' var(--text-color)', fontWeight: '400'}}>arrow_back</span>
+      </button>
+
+      <p className="text-end mt-4 text-(--white-text) text-xl">
+        selected: 0/3
+      </p>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
