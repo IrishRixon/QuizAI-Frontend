816a0046bb3f97a4dc36a05b678cace9aade9591
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-11-12 21:37:45 +0800

    `Added react-confetti-explosion package and imported it in Score component`

--- src/Components/Score/Score.tsx
+++ src/Components/Score/Score.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react"
 import { ScoreContext, type ScoreContextType } from "../../Context/ScoreContext"
 import { CategoryContext, type StateCat } from "../../Context/CategoryContext";
+import ConfettiExplosion from 'react-confetti-explosion';
 
 function Score() {
     const { score, setScore} = useContext(ScoreContext) as ScoreContextType;
@@ -8,6 +9,7 @@ function Score() {
     
   return (
     <main className="p-6 w-full h-full z-10 relative flex flex-col justify-center items-center">
+      <ConfettiExplosion />
         <p className="text-(--white-text)">Your score is: </p>
         <h1 className="text-7xl text-(--white-text)">{score}/{categoriesSelected.numberOfQuestions}</h1>
     </main>
