6730aaa6d591af299482b4b30bc5261f0828ad67
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-11-15 10:57:37 +0800

    feat: add buttons for navigation to play again and main menu on score page

---
+++ src/Components/Score/Button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  label: string;
+  handleClick: () => void;
+}
+
+function Button({ label, handleClick }: Props) {
+  return (
+    <button className="h-[64px] w-[200px] flex p-4 text-(--text-color) border border-(--secondary-color) rounded justify-center"
+    onClick={handleClick}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
--- src/Components/Score/Score.tsx
+++ src/Components/Score/Score.tsx
@@ -2,16 +2,27 @@ import { useContext } from "react"
 import { ScoreContext, type ScoreContextType } from "../../Context/ScoreContext"
 import { CategoryContext, type StateCat } from "../../Context/CategoryContext";
 import ConfettiExplosion from 'react-confetti-explosion';
+import Button from "./Button";
+import { useNavigate } from "react-router";
 
 function Score() {
     const { score, setScore} = useContext(ScoreContext) as ScoreContextType;
     const { categoriesSelected } = useContext(CategoryContext) as StateCat;
+    const navigate = useNavigate();
     
   return (
-    <main className="p-6 w-full h-full z-10 relative flex flex-col justify-center items-center">
+    <main className="p-6 w-full h-full z-10 relative flex flex-col justify-center items-center gap-8">
+      <div className="flex flex-col justify-center items-center">
       <ConfettiExplosion />
         <p className="text-(--white-text)">Your score is: </p>
         <h1 className="text-7xl text-(--white-text)">{score}/{categoriesSelected.numberOfQuestions}</h1>
+      </div>
+      
+      <div className="flex flex-col gap-4">
+      <Button label="Play Again" handleClick={() => navigate("/categories")}></Button>
+      <Button label="Main Menu" handleClick={() => navigate("/")}></Button>
+      </div>
+        
     </main>
   )
 }
