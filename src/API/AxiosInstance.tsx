71fa211c2a57484317c777b960d79c967dc4993c
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-11-25 15:31:24 +0800

    fix: update API base URL to new endpoint

--- src/API/AxiosInstance.tsx
+++ src/API/AxiosInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'https://quizai-backend-adzt.onrender.com',
+    baseURL: 'https://quizai-backend-9q17.onrender.com',
     headers: {
         "Content-Type": "application/json"
     }
