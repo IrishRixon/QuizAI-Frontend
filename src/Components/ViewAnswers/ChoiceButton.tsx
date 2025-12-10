538964a328e2ee4ae29bc7e3f15278c0bb08ce54
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-12-10 20:23:45 +0800

    feat: enhance ChoiceButton styles and add navigation buttons in ViewAnswers component

--- src/Components/ViewAnswers/ChoiceButton.tsx
+++ src/Components/ViewAnswers/ChoiceButton.tsx
@@ -11,19 +11,19 @@ function ChoiceButton({ index, label, chosen, correctAns }: Props) {
   let color;
 
   if(correctAns === index) {
-    color = "bg-green-800";
+    color = "bg-green-800 border-green-800 text-shadow-red-200";
   }
   else if(chosen === index){
-    color = "bg-red-900";
+    color = "bg-red-900 border-red-900 text-shadow-red-200";
   }
   else {
-    color = " ";
+    color = "text-(--text-color)";
   }
   return (
     <button
       className={`p-ripple w-full border border-(--secondary-color) p-4 rounded ${color}`}
     >
-      <p className="text-xl text-(--text-color) text-center">{label}</p>
+      <p className="text-xl text-center">{label}</p>
       <Ripple />
     </button>
   );
--- src/Components/ViewAnswers/ViewAnswers.tsx
+++ src/Components/ViewAnswers/ViewAnswers.tsx
@@ -12,11 +12,13 @@ function ViewAnswers() {
 
   return (
     <main className="h-full w-full p-6 relative z-10 flex flex-col sm:px-28 md:px-40 lg:px-52 xl:px-[450px]">
-      <div className="flex">
+      <div className="flex mb-8">
         <button
           type="button"
           className="flex active:bg-(--secondary-color) active:text-(--white-text) rounded-sm p-ripple"
-          onClick={() => navigate("/score", {state: {questions: questions}})}
+          onClick={() =>
+            navigate("/score", { state: { questions: questions } })
+          }
         >
           <span
             className="material-symbols-rounded"
@@ -32,13 +34,63 @@ function ViewAnswers() {
         </button>
       </div>
 
+      <div className="flex w-full justify-between mb-4">
+        <button
+          type="button"
+          className="flex active:bg-(--secondary-color) active:text-(--white-text) rounded-sm p-ripple border border-(--secondary-color) w-[80px] justify-center"
+          onClick={() => {
+            setCurrentQuestionIndex((prev) => {
+              const updated = prev - 1;
+              return updated >= 0 ? updated : prev;
+            });
+          }}
+        >
+          <span
+            className="material-symbols-rounded"
+            style={{
+              fontSize: "40px",
+              color: "var(--text-color)",
+              fontWeight: "400",
+            }}
+          >
+            chevron_backward
+          </span>
+          <Ripple />
+        </button>
+
+        <button
+          type="button"
+          className="flex active:bg-(--secondary-color) active:text-(--white-text) rounded-sm p-ripple border border-(--secondary-color) w-[80px] justify-center"
+          onClick={() => {
+            setCurrentQuestionIndex((prev) => {
+              const updated = prev + 1;
+              return updated < questions.length ? updated : prev;
+            });
+          }}
+        >
+          <span
+            className="material-symbols-rounded"
+            style={{
+              fontSize: "40px",
+              color: "var(--text-color)",
+              fontWeight: "400",
+            }}
+          >
+            chevron_forward
+          </span>
+          <Ripple />
+        </button>
+      </div>
+
       <section className="grow">
-        <div className="h-[273px] w-full rounded shadow-md bg-(--secondary-color) p-2.5 flex items-center justify-center mb-9">
+        <div className="h-[273px] w-full rounded shadow-md bg-(--secondary-color) p-2.5 flex items-center justify-center mb-9 relative">
           <p className="text-(--white-text) text-xl text-center">
             {questions.length > currentQuestionIndex
               ? questions[currentQuestionIndex].question
               : ""}
           </p>
+
+          {questions[currentQuestionIndex].chosen == questions[currentQuestionIndex].answer && <span className="absolute right-2 top-0 text-2xl italic text-green-400">+1</span>}
         </div>
 
         <section className="flex flex-col gap-2 overflow-y-auto pb-4">
