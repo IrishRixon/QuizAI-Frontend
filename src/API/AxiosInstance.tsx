14d6f46ecd0e48e540d78c975418b816f4cb2fbc
Irish Rixon Dela Pena <irishrixon@gmail.com>
2025-12-10 20:24:14 +0800

    fix: update API base URL to production endpoint

--- src/API/AxiosInstance.tsx
+++ src/API/AxiosInstance.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    // baseURL: 'https://quizai-backend-9q17.onrender.com',
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://quizai-backend-9q17.onrender.com',
+    // baseURL: 'http://localhost:3000',
     headers: {
         "Content-Type": "application/json"
     }
