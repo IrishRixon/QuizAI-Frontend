caebda8e92056018d9df4247491df3115761138c
Irish Rixon Dela Pena <irishrixon@gmail.com>
2026-01-07 16:35:22 +0800

    `Update socket URL to production environment`

--- src/socket.ts
+++ src/socket.ts
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.PROD ? undefined : 'http://localhost:3000';
+// const URL = import.meta.env.PROD ? undefined : 'http://localhost:3000';
+const URL = 'https://quizai-backend-9q17.onrender.com';
 
 export const socket = io(URL, {
     autoConnect: false
